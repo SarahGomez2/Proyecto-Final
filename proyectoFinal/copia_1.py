ce7f824297b2fa2229e81613e8282656ca4d2b58
SarahGomez2 <160065127+SarahGomez2@users.noreply.github.com>
2024-05-24 21:55:43 -0600

    Update copia_1.py

--- proyectoFinal/copia_1.py
+++ proyectoFinal/copia_1.py
@@ -234,4 +234,49 @@ if __name__ == "__main__":
 
     print(nomina)
 
+REPORTEC= 'cuentas.txt'
+REPORTEE= 'ejecutivos.txt'
+REPORTEA= 'aperturas.txt'
+#definimos el programa
+def banca():
+#Colocamos los condicionales para que el usuario interactue con el menu
+    preguntar = True
+    while preguntar:
+
+        opcion = input("Seleccione una opción:")
+        opcion = int(opcion)
+print(banca())
+#Vamos a agragar los constructores ya creados (colocar en la parte de print)
+    elif1 opcion == "1":
+        print("1) Cuentas")
+        preguntar = False
+    elif opcion == "2":
+        print("2) Ejecutivos")
+        preguntar = False
+    elif opcion == "3":
+        print("3) Generar reportes")
+        preguntar = False
+    elif opcion == "4":
+        print("4) Salir")
+        preguntar = False
+    else:
+        print("Opción invalida, volver a preguntar :))")
+        preguntar = True
+#Aquí vamos a colocar los constructores
+
+
+#Colocar el constructor para abrir los reportes
+
+        # Colocar la parte de agregar cuentas 1
+def mostrar_menu():
+    print("Seleccione apartado que quiera visitar")
+    print("1) Cuentas")
+    print("2) Ejecutivos")
+    print("3) Generar reportes")
+    print("4) Cerrar")
+#mandar a llamar solo con su nombre,  las validaciones.
+print(mostrar_menu())
+
+
+
 
