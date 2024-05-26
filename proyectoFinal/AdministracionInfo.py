72ac73ba8c2cb9a003de235b82dda494a740360d
SarahGomez2 <160065127+SarahGomez2@users.noreply.github.com>
2024-05-26 04:31:17 -0600

    Update and rename AdministracionInfo.py to PruebaAdmi.py

--- proyectoFinal/AdministracionInfo.py
+++
@@ -1,6 +0,0 @@
-#Programa:Proyecto final 
-#Objetivo: Crear menú principal de la institucion bancaria 
-#Autor: Binary Balance
-#Fecha: 26/05/2024
-
-
---
+++ proyectoFinal/PruebaAdmi.py
@@ -0,0 +1,43 @@
+#Programa:Proyecto final 
+#Objetivo: Crear menú principal de la institucion bancaria 
+#Autor: Binary Balance
+#Fecha: 26/05/2024
+
+def banca():
+#Colocamos los condicionales para que el usuario interactue con el menu
+    preguntar = True
+    while preguntar:
+
+        opcion = input("Seleccione una opción:")
+        opcion = int(opcion)
+print(banca())
+#Vamos a agragar los constructores ya creados (colocar en la parte de print)
+    elif1 opcion == "1":
+        print("1) Cuentas")
+        preguntar = False
+    elif opcion == "2":
+        print("2) Ejecutivos")
+        preguntar = False
+    elif opcion == "3":
+        print("3) Generar reportes")
+        preguntar = False
+    elif opcion == "4":
+        print("4) Salir")
+        preguntar = False
+    else:
+        print("Opción invalida, volver a preguntar :))")
+        preguntar = True
+#Aquí vamos a colocar los constructores
+
+
+#Colocar el constructor para abrir los reportes
+
+        # Colocar la parte de agregar cuentas 1
+def mostrar_menu():
+    print("Seleccione apartado que quiera visitar")
+    print("1) Cuentas")
+    print("2) Ejecutivos")
+    print("3) Generar reportes")
+    print("4) Cerrar")
+#mandar a llamar solo con su nombre,  las validaciones.
+print(mostrar_menu())
